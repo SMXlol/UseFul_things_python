5f6dfff0242da13429ff11e62151693bf00b53b3
SMXlol <142971728+SMXlol@users.noreply.github.com>
2023-11-28 00:54:38 -0200

    Create selection_sort.py
    select

---
+++ Useful/selection_sort.py
@@ -0,0 +1,9 @@
+def selection_sort(nums):
+    for i in range(len(nums)):
+            lowest_value_index = i
+            for j in range(i + 1, len(nums)):
+                if nums[j] < nums[lowest_value_index]:
+                    lowest_value_index = j
+            nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
+
+selection_sort(random_list_of_nums)
